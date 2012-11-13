6530259df84781eb3426fc4c8af2cf3c56fb2f2f
liuqi <keyboarder5211@gmail.com>
2012-11-13 19:41:28 +0800

    initial basepro

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Basepro::Application.config.secret_token = '5afbdbc1f9b26f730ee891f7970f2d1b1012d918b93c34fa215a5d61fd746bbcc4c490fedd9914289f8371be487f090d24f3fd82cd55adaa9ec74e503b638a52'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Basepro::Application.config.session_store :cookie_store, key: '_basepro_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Basepro::Application.config.session_store :active_record_store
