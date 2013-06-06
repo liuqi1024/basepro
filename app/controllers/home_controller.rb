13677f663d60fa50fe3bf5b9790a9d0c41d0be34
liuqi1024 <keyboarder5211@gmail.com>
2013-06-06 12:12:57 +0800

    initial basepro

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,22 +4,24 @@ class HomeController < ApplicationController
   def index
     # raise current_user.auth.to_yaml
     return unless user_signed_in? 
-    @provider = current_user.provider
-    @uid      = current_user.uid
-    @token    = current_user.auth.credentials.token
-    @expires_at = current_user.auth.credentials.expires_at
+    # @provider = current_user.provider
+    # @uid      = current_user.uid
+    # @token    = current_user.auth.credentials.token if current_user.auth.credentials
+    # @expires_at = current_user.auth.credentials.expires_at if current_user.auth.credentials
     @name = current_user.name
-    @avatar = case current_user.provider
-              when "weibo"
-                avatar_weibo
-              when "twitter"
-                avatar_twitter
-              when "qq_connect"
-                avatar_qq
-              when "github"
-                avatar_github
-              else
-              end
+    # @avatar = case current_user.provider
+    #           when "weibo"
+    #             avatar_weibo
+    #           when "twitter"
+    #             avatar_twitter
+    #           when "qq_connect"
+    #             avatar_qq
+    #           when "github"
+    #             avatar_github
+    #           else
+    #           end
+    
+    
   end
   
   private
