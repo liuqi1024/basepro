dcd2a3399fc5fc2ad545ea2a92191de54162e12b
liuqi1024 <keyboarder5211@gmail.com>
2013-07-05 09:17:26 +0800

    add capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,70 @@
+set :application, "basepro"
+
+# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+set :scm, :git
+set :repository,  "https://github.com/liuqi1024/basepro.git"
+
+
+# role :web, "your web-server here"                          # Your HTTP server, Apache/etc
+# role :app, "your app-server here"                          # This may be the same as your `Web` server
+# role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
+
+default_run_options[:pty] = true 
+
+
+server "10.0.2.26", :app, :web, :db, :primary => true
+set :user, "liuqi"
+# set :scm_passphrase, "myehome"  # The deploy user's password
+
+set :use_sudo, false
+
+set :deploy_to, "/Users/liuqi/deployment/basepro"
+
+set :normalize_asset_timestamps, false
+
+set :deploy_via, :remote_cache
+
+namespace :deploy do
+
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+  
+  task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+  after "deploy:finalize_update", "deploy:symlink_config"
+end
+
+
+
+# set :application, "basepro"
+# set :repository,  "git@github.com:liuqi1024/basepro.git"
+
+# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+# role :web, "your web-server here"                          # Your HTTP server, Apache/etc
+# role :app, "your app-server here"                          # This may be the same as your `Web` server
+# role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
