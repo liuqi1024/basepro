d2640d95189d017e1937ec236d1f33d20a1a973f
liuqi1024 <keyboarder5211@gmail.com>
2013-07-27 11:32:48 +0800

    test

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -12,7 +12,7 @@ stderr_path "#{Rails.root}/log/unicorn.log"
 stdout_path "#{Rails.root}/log/unicorn.log"
 
 listen "#{Rails.root}/tmp/unicorn.todo.sock"
-listen 8080
+# listen 8080
 
 worker_processes 5
 timeout 30
